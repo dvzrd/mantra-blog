0b94d782d042e0352396ba2d864db2c46c8285da
Damir <damir@withpulp.com>
2016-01-31 18:30:42 -0500

    index.js prep

--- client/components/layouts.main/index.jsx
+++ client/components/layouts.main/index.jsx
@@ -4,16 +4,16 @@ import React from 'react';
 const Layout = ({content = () => null }) => (
   <div>
     <header>
-    <h1>Mantra Voice</h1>
-    <Navigation />
+      <h1>Mantra Voice</h1>
+      <Navigation />
     </header>
 
     <div>
-    {content()}
+      {content()}
     </div>
 
     <footer>
-    <small>Built with <a href='https://github.com/kadirahq/mantra'>Mantra</a> & Meteor.</small>
+      <small>Built with <a href='https://github.com/kadirahq/mantra'>Mantra</a> & Meteor.</small>
     </footer>
   </div>
 );
