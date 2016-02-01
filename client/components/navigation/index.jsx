78681b79026fd6072ad2d0ed10df790449010a7d
Damir <damir@withpulp.com>
2016-01-31 20:05:14 -0500

    wip

--- client/components/navigation/index.jsx
+++ client/components/navigation/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Navigation = () => (
-  <div>
+  <nav>
     <b> Navigation: </b>
     <a href="/">Home</a> |
     <a href="/new-post">New Post</a>
-  </div>
+  </nav>
 );
 
 export default Navigation;
